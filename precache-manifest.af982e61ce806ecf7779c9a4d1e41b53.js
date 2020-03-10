b0e55e723e038f653e1fa94f93aceb029e8ac842
ziliana <tofu.ball1999@gmail.com>
2020-03-10 20:41:23 +0100

    Updates

---
+++ precache-manifest.af982e61ce806ecf7779c9a4d1e41b53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "028569ebf0ca85f37b6c2f37a15d2ab1",
+    "url": "/monster-site/index.html"
+  },
+  {
+    "revision": "8b5aa9374a8711037974",
+    "url": "/monster-site/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9d8ce3d5b385974b5c9e",
+    "url": "/monster-site/static/js/2.3d4badca.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monster-site/static/js/2.3d4badca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b5aa9374a8711037974",
+    "url": "/monster-site/static/js/main.32534484.chunk.js"
+  },
+  {
+    "revision": "2a7d395b9e2d7fed4aa3",
+    "url": "/monster-site/static/js/runtime-main.79321d2a.js"
+  }
+]);
